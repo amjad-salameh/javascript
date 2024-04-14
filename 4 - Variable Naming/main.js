7d72fc5018cda1c9d2a722793e5b201ecf7f63e1
amjad-salameh <amjadsalameh00@gmail.com>
2024-04-14 20:56:52 +0300

    4 - Variable Naming

---
+++ 4 - Variable Naming/main.js
@@ -0,0 +1,10 @@
+const paris = 35;
+console.log(paris);
+const isLogIn = true;
+console.log(isLogIn);
+const isLogOut = false;
+console.log(isLogOut);
+const emailAddress = "example.com";
+console.log(emailAddress);
+const basketBallGame = 90;
+console.log(basketBallGame);
