e66b69104f8e448e69ab39c3a5e41c8d8e1a4982
amjad-salameh <amjadsalameh00@gmail.com>
2024-04-14 14:49:12 +0300

    3 - Primitive Types

---
+++ 3 - Primitive Types/main.js
@@ -0,0 +1,15 @@
+//1
+let name = "amjad";
+//2
+let num1 = 1;
+let num2 = 2;
+let num3 = num1 + num2;
+//3
+document.write(Number.isSafeInteger(3));
+//4
+let x;
+document.write(x);
+document.write(num3);
+//5
+document.write("5" / "amjad");
+
