2b699875497d7025b0b7f6150e502b31be3b2029
amjad-salameh <amjadsalameh00@gmail.com>
2024-04-15 12:33:22 +0300

    2 - typeof Operator

---
+++ 2 - typeof Operator/main.js
@@ -0,0 +1,27 @@
+//-------------1---------------------------------
+const num1 = 10;
+const str = "hello"
+const bool = true;
+console.log(typeof num1);
+console.log(typeof str);
+console.log(typeof bool);
+//-------------2---------------------------------
+const firstVar = 42;
+const secondVar = "42";
+console.log(typeof firstVar);
+console.log(typeof secondVar);
+
+//-------------3---------------------------------   
+const noValue = undefined;
+const emptyValue = null;
+console.log(typeof noValue);
+console.log(typeof emptyValue);
+
+//-------------4---------------------------------   
+
+const num2 = 10;
+const str2 = "hello";
+const bool2 = true;
+console.log(`the type of num2 is ${typeof num2}`);
+console.log(`the type of num2 is ${typeof str2}`);
+console.log(`the type of num2 is ${typeof bool2}`);
