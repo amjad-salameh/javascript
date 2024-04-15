ecc2ae550dd5a75872beaaf77fade8b190c1656e
amjad-salameh <amjadsalameh00@gmail.com>
2024-04-15 12:21:39 +0300

    6 - Template Literals

---
+++ 6 - Template Literals/main.js
@@ -0,0 +1,26 @@
+//-------------------------------------1----------------------------------------
+const name = "amjad";
+const age = 27;
+console.log(`my name is ${name} and I am ${age} years old`);
+//---------------------2---------------------------------------
+const length = 5;
+const width = 10;
+console.log(length * width);
+//-----------------------------------3-------------------------------------
+const today = "monday";
+const x = "swim";
+const y = "read";
+const z = "eat";
+console.log(`in ${today} i have ${x} and ${y} and ${z}`);
+//-----------------------------------4--------------------------------
+const animal1= "cat";
+const animal2= "dog";
+const animal3= "fish";
+
+console.log(`i have ${animal1} and ${animal2} and ${animal3}`);
+//-----------------------------------5--------------------------------
+const q = 10;
+const w = 20;
+const e = 30;
+
+console.log(q * w * e);
