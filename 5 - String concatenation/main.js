58cea518950f4f29b2bf030ff27200c53dcaa590
amjad-salameh <amjadsalameh00@gmail.com>
2024-04-15 12:50:09 +0300

    5 - String concatenation

---
+++ 5 - String concatenation/main.js
@@ -0,0 +1,23 @@
+//------------------1---------------------
+const name = "amjad";
+const age = 27;
+
+console.log(`my name is ${name} and my age is ${age}` ) ;
+
+//------------------2---------------------
+const food = "pizza";
+const movies = "split";
+const color = "red";
+
+console.log(`my favorite food is ${food} and my favorite movie is ${movies} and my favorite color is ${color}` ) ;
+
+//------------------3---------------------
+const a = 10;
+const b = 20;
+
+console.log(`the sum of ${a} and ${b} is ${a+b}` ) ;
+
+//------------------4---------------------
+let today = "monday";
+console.log(`the today is  ${"monday"} `) ;
+
