f9307166fec519ea1313f16b29634560f46591f3
amjad-salameh <amjadsalameh00@gmail.com>
2024-04-16 15:53:15 +0300

    12-Type Conversion

---
+++ 12-Type Conversion/main.js
@@ -0,0 +1,75 @@
+//------------------1-------------------
+const number = 123;
+const stringNumber = number.toString();
+const stringNumberStringConcat = number +'';
+console.log(stringNumberStringConcat);
+console.log(typeof stringNumberStringConcat);
+
+//------------------2-------------------
+const string ="456";
+const numberString = parseInt (string);
+console.log(numberString);
+console.log(typeof numberString);
+
+//------------------3-------------------
+const float = "78.9";
+const numberFloat = parseFloat(float);
+console.log(numberFloat);
+console.log(typeof numberFloat);
+
+//------------------4-------------------
+const num = 5;
+const str ="3";
+const numStr = num + parseInt(str);
+console.log(numStr);
+
+//------------------5-------------------
+const num1 = 0;
+const num2 = 1;
+const Boolean1 = Boolean(0);
+const Boolean2 = Boolean(1);
+console.log(Boolean1);
+console.log(Boolean2);
+
+//------------------6-------------------
+const strring = "24";
+const numString = +strring;
+console.log( typeof numString);
+
+//------------------7-------------------
+const boolean3 = true;
+const boolean4 = false;
+const numt = Number( boolean3);
+const numf = Number( boolean4);
+console.log(numt);
+console.log(numf);
+
+//------------------8-------------------
+const nnull = null;
+const numNull = Number(nnull);
+console.log(numNull);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
