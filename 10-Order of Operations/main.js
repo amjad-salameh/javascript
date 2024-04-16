ea3cfecda389d331b7c7d23584fcc33588fd6475
amjad-salameh <amjadsalameh00@gmail.com>
2024-04-16 12:35:48 +0300

    10-Order of Operations

---
+++ 10-Order of Operations/main.js
@@ -0,0 +1,82 @@
+//---------------1------------------------
+const a = 8;
+const b =2;
+console.log(a*b+3);
+
+//---------------2------------------------
+const x =4;
+const y =2;
+const z =3;
+console.log(x+y*z-1);
+
+//---------------3------------------------
+const a1= 4;
+const b1=2;
+console.log((a1+b1)*(a1-b1));
+
+//---------------4------------------------
+const num = 50;
+const x1 = 5;
+const y1 = 6;
+console.log(num-x1*y1);
+
+//---------------5------------------------
+const a2 = 8;
+const b2 = 2;
+const c = 4;
+console.log(a2/b2*c);
+
+//---------------6------------------------
+const a3 = 2;
+const b3 = 3;
+const c1 = 4;
+console.log(a3+b3*c1);
+console.log((a3+b3)*c1);
+
+//---------------7------------------------
+const a4 = 2;
+const b4 = 3;
+const c2 = 5;
+console.log(((a4+b4)*c2)/2);
+
+//---------------8------------------------
+
+const a5 = 3;
+const b5 = 6;
+const c3 = 5;
+const d = 4;
+console.log(a5+b5*(c3+d)/b5-d);
+
+//---------------9------------------------
+const num1 = 2;
+const str ='3';
+console.log(num1+str*4);
+
+//---------------10------------------------
+const a6 = 10;
+const b6 = 20;
+const c4 = "30";
+console.log(a6+b6+c4);
+console.log(c4+a6+b6);
+
+//---------------11------------------------
+const numStr = '3';
+const num2 = 10;
+console.log(+numStr + num2);
+
+//---------------12------------------------
+let x2 = 10;
+console.log(x2++ * 2);
+
+//---------------13------------------------
+const a7 = 10;
+console.log(a7/0);//Infinity
+
+//---------------14------------------------
+
+const a8 = 15;
+const b8 = 4;
+console.log(a8%b8);
+
+
+
