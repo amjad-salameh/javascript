e90fa1ffd29bc055adee6ce2065c6fbe82bec1af
amjad-salameh <amjadsalameh00@gmail.com>
2024-05-16 15:51:36 +0300

    Exercise 5 - Paragraph Highlighter

---
+++ Exercise 5 - Paragraph Highlighter/main.js
@@ -0,0 +1,11 @@
+let btn = document.getElementById('btn');
+let input = document.getElementById("colorPicker");
+let p = document.getElementById('p');
+btn.onclick = function(){
+   input.style.backgroundColor = 'red';
+}
+  function click(){
+   p.style.backgroundColor = 'blue';
+}
+
+
